5cc8ecac4cfdc5d20a35feaa6c7ad237cdb246c3
avargas <avargas@frontuari.com>
2017-06-22 14:43:15 -0400

    Arreglamdo problema de funciones vacias en editores para que no se muestren si estan vacios o no hagan nimguna accion

--- includes/wpematicohk_execute_action_filter.php
+++ includes/wpematicohk_execute_action_filter.php
@@ -3,18 +3,20 @@
 			if(!isset($_POST['action'])){
 				$wpematicohk_options = get_option('wpematicohk_datahooks');
 				for($i=0; $i<count($wpematicohk_options['wpematicohk_options_action_filters']);$i++){
-				 	$wpematicohk_all_function_filters = explode(',',$wpematicohk_options['wpematicohk_functions_action_filter'][$i]);
-				 	for($j=0; $j<count($wpematicohk_all_function_filters);$j++){
-				 		if($wpematicohk_options['wpematicohk_type_hook']!='filter'){
-				 			add_action($wpematicohk_options['wpematicohk_options_action_filters'][$i],$wpematicohk_all_function_filters[$j],10,0);
-				 		}else{
-				 			add_filter($wpematicohk_options['wpematicohk_options_action_filters'][$i],$wpematicohk_all_function_filters[$j],10,$wpematicohk_options['wpematicohk_functions_parameters'][$i]);
-				 		}//close type hook
-				 	}
-				 	$wpematicohk_code = $wpematicohk_options['wpematicohk_options_functions'][$i];	
-					eval($wpematicohk_code);
+					if($wpematicohk_options['wpematicohk_options_functions'][$i]!=''){
+					 	$wpematicohk_all_function_filters = explode(',',$wpematicohk_options['wpematicohk_functions_action_filter'][$i]);
+					 	for($j=0; $j<count($wpematicohk_all_function_filters);$j++){
+					 		if($wpematicohk_options['wpematicohk_type_hook']!='filter'){
+					 			add_action($wpematicohk_options['wpematicohk_options_action_filters'][$i],$wpematicohk_all_function_filters[$j],10,0);
+					 		}else{
+					 			add_filter($wpematicohk_options['wpematicohk_options_action_filters'][$i],$wpematicohk_all_function_filters[$j],10,$wpematicohk_options['wpematicohk_functions_parameters'][$i]);
+					 		}//close type hook
+					 	}
+					 	$wpematicohk_code = $wpematicohk_options['wpematicohk_options_functions'][$i];	
+						eval($wpematicohk_code);
+					}
 					
-				}
+				}//cierre del for
 			}
 		}//closed get tab
 
