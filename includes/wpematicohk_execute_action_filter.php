e1dbca521d9fd8d4a58ce3c96217b8ee9a5bbef6
avargas <avargas@frontuari.com>
2017-06-26 00:51:20 -0400

    Arreglando notices, variables declaradas, estandarizando mas el codigo

--- includes/wpematicohk_execute_action_filter.php
+++ includes/wpematicohk_execute_action_filter.php
@@ -4,24 +4,26 @@
 				$wpematicohk_options = array();
 				$wpematicohk_all_function_filters = array();
 				$wpematicohk_options = get_option('wpematicohk_datahooks');
-				foreach ($wpematicohk_options['wpematicohk_options_action_filters'] as $i => $value) {
-					if($wpematicohk_options['wpematicohk_options_functions'][$i]!=''){
-					 	if(isset($wpematicohk_options['wpematicohk_functions_action_filter'][$i])){
-					 		$wpematicohk_all_function_filters = explode(',',$wpematicohk_options['wpematicohk_functions_action_filter'][$i]);
-					 	}
-					 	if(!empty($wpematicohk_all_function_filters[0])){
-						 	for($j=0; $j<count($wpematicohk_all_function_filters);$j++){
-						 		if($wpematicohk_options['wpematicohk_type_hook'][$i]!='filter'){
-						 			add_action($wpematicohk_options['wpematicohk_options_action_filters'][$i],$wpematicohk_all_function_filters[$j],10,$wpematicohk_options['wpematicohk_functions_parameters'][$i]);
-						 		}else{
-						 			add_filter($wpematicohk_options['wpematicohk_options_action_filters'][$i],$wpematicohk_all_function_filters[$j],10,$wpematicohk_options['wpematicohk_functions_parameters'][$i]);
-						 		}//close type hook
-						 	}
-						 	$wpematicohk_code = $wpematicohk_options['wpematicohk_options_functions'][$i];	
-							eval($wpematicohk_code);
-						}//closed if isset wpematico all functions filters
-					}
-				}//cierre del for
+				if(isset($wpematicohk_options['wpematicohk_options_action_filters'])){
+					foreach ($wpematicohk_options['wpematicohk_options_action_filters'] as $i => $value) {
+							if($wpematicohk_options['wpematicohk_options_functions'][$i]!=''){
+							 	if(isset($wpematicohk_options['wpematicohk_functions_action_filter'][$i])){
+							 		$wpematicohk_all_function_filters = explode(',',$wpematicohk_options['wpematicohk_functions_action_filter'][$i]);
+							 	}
+							 	if(!empty($wpematicohk_all_function_filters[0])){
+								 	for($j=0; $j<count($wpematicohk_all_function_filters);$j++){
+								 		if($wpematicohk_options['wpematicohk_type_hook'][$i]!='filter'){
+								 			add_action($wpematicohk_options['wpematicohk_options_action_filters'][$i],$wpematicohk_all_function_filters[$j],10,$wpematicohk_options['wpematicohk_functions_parameters'][$i]);
+								 		}else{
+								 			add_filter($wpematicohk_options['wpematicohk_options_action_filters'][$i],$wpematicohk_all_function_filters[$j],10,$wpematicohk_options['wpematicohk_functions_parameters'][$i]);
+								 		}//close type hook
+								 	}
+								 	$wpematicohk_code = $wpematicohk_options['wpematicohk_options_functions'][$i];	
+									eval($wpematicohk_code);
+								}//closed if isset wpematico all functions filters
+							}
+						}//cierre del for
+				}//isset array foreach
 			}
 		}//closed get tab
 ?>
